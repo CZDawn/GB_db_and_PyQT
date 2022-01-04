f2637ce44c984e79623d9588bb5c96c524db8394
CZDawn <codezerodawn@gmail.com>
2022-01-04 15:31:25 +0300

    Done home task for lesson 2

--- client.py
+++ client.py
@@ -13,86 +13,93 @@ from common.variables import DEFAULT_PORT, DEFAULT_IP_ADDRESS, ACTION, \
                              MESSAGE_TEXT, SENDER, RECIPIENT, EXIT
 from common.utils import send_message, get_message
 from errors import ServerError, ReqFieldMissingError, IncorrectDataReceivedError
+from metaclasses import ClientVerifier
 
 
 LOG = getLogger('client_logger')
 
 
-@log_decorator
-def server_message_handler(sock, username):
-    while True:
+class ClientSender(Thread, metaclass=ClientVerifier):
+    def __init__(self, username, socket):
+        self.username = username
+        self.socket = socket
+        super().__init__()
+
+    def exit_message(self):
+        return {
+            ACTION: EXIT,
+            TIME: time(),
+            USER: self.username
+        }
+
+    def create_message(self):
+        addressee = input('Enter addressees username: ')
+        message = input('Enter your message: ')
+        dict_message = {
+            ACTION: MESSAGE,
+            TIME: time(),
+            SENDER: self.username,
+            RECIPIENT: addressee,
+            MESSAGE_TEXT: message
+        }
+        LOG.debug(f'Created dict-message: {dict_message}')
         try:
-            message = get_message(sock)
-            if ACTION in message and message[ACTION] == MESSAGE and TIME in message and SENDER in message and RECIPIENT in message and MESSAGE_TEXT in message and message[RECIPIENT] == username:
-                message_time = strftime("%d.%m.%Y %H:%m:%S", strptime(ctime(message[TIME])))
-                print(f'{message_time} - {message[SENDER]}: {message[MESSAGE_TEXT]}')
-                LOG.info(
-                    f'User {username} obtain the message {message[MESSAGE_TEXT]} '
-                    f'from the user {message[SENDER]}'
-                )
-            elif message[RESPONSE] == 300:
-                LOG.debug(
-                    f'Obtained answer "Response 300: {message[ERROR]}".'
-                )
-                print(f'{message[ERROR]}')
-            else:
-                LOG.error(
-                    f'Incorrect message "{message}" obtained'
-                )
-        except IncorrectDataReceivedError:
-            LOG.error('Decoding of obtained message failed.')
-        except (ConnectionError, ConnectionRefusedError, ConnectionAbortedError, OSError, JSONDecodeError):
+            send_message(self.socket, dict_message)
+            LOG.info(f'Message sended to user {addressee}')
+        except:
             LOG.critical(f'Lost connection with server')
-            break
+            sys.exit(1)
+
+    def run(self):
+        print('Commands:')
+        print('1 - send message.')
+        print('2 - exit.')
+
+        while True:
+            command = input('Make your choice: ')
+            if command == '1':
+                self.create_message()
+            elif command == '2':
+                send_message(self.sock, self.exit_message())
+                print('Program is finished.')
+                LOG.info(f'Client {username} stop working.')
+                sleep(0.5)
+                break
+            else:
+                print('Unknown command.')
 
 
-@log_decorator
-def create_message(sock, username):
-    addressee = input('Enter addressees username: ')
-    message = input('Enter your message: ')
-    dict_message = {
-        ACTION: MESSAGE,
-        TIME: time(),
-        SENDER: username,
-        RECIPIENT: addressee,
-        MESSAGE_TEXT: message
-    }
-    LOG.debug(f'Created dict-message: {dict_message}')
-    try:
-        send_message(sock, dict_message)
-        LOG.info(f'Message sended to user {addressee}')
-    except:
-        LOG.critical(f'Lost connection with server')
-        sys.exit(1)
+class ClientReader(Thread, metaclass=ClientVerifier):
+    def __init__(self, username, socket):
+        self.username = username
+        self.socket = socket
+        super().__init__()
 
-
-@log_decorator
-def exit_message(username):
-    return {
-        ACTION: EXIT,
-        TIME: time(),
-        USER: username
-    }
-
-
-@log_decorator
-def user_menu(sock, username):
-    print('Commands:')
-    print('1 - send message.')
-    print('2 - exit.')
-
-    while True:
-        command = input('Make your choice: ')
-        if command == '1':
-            create_message(sock, username)
-        elif command == '2':
-            send_message(sock, exit_message(username))
-            print('Program is finished.')
-            LOG.info(f'Client {username} stop working.')
-            sleep(0.5)
-            break
-        else:
-            print('Unknown command.')
+    def run(self):
+        while True:
+            try:
+                message = get_message(self.socket)
+                if ACTION in message and message[ACTION] == MESSAGE and TIME in message and SENDER in message and RECIPIENT in message and MESSAGE_TEXT in message and message[RECIPIENT] == self.username:
+                    message_time = strftime("%d.%m.%Y %H:%m:%S", strptime(ctime(message[TIME])))
+                    print(f'{message_time} - {message[SENDER]}: {message[MESSAGE_TEXT]}')
+                    LOG.info(
+                        f'User {self.username} obtain the message {message[MESSAGE_TEXT]} '
+                        f'from the user {message[SENDER]}'
+                    )
+                elif message[RESPONSE] == 300:
+                    LOG.debug(
+                        f'Obtained answer "Response 300: {message[ERROR]}".'
+                    )
+                    print(f'{message[ERROR]}')
+                else:
+                    LOG.error(
+                        f'Incorrect message "{message}" obtained'
+                    )
+            except IncorrectDataReceivedError:
+                LOG.error('Decoding of obtained message failed.')
+            except (ConnectionError, ConnectionRefusedError, ConnectionAbortedError, OSError, JSONDecodeError):
+                LOG.critical(f'Lost connection with server')
+                break
 
 
 @log_decorator
@@ -135,17 +142,17 @@ def args_parser():
 def main():
     server_ip, server_port, client_name = args_parser()
     if not client_name:
-        client_name = input('Введите имя пользователя: ')
+        client_name = input('Enter username: ')
     LOG.info(
         f'Client app connected. Server IP-address: {server_ip}, '
         f'server port: {server_port}, username: {client_name}.'
     )
 
     try:
-        client_sock = socket(AF_INET, SOCK_STREAM)
-        client_sock.connect((server_ip, server_port))
-        send_message(client_sock, confirm_presence(client_name))
-        answer = receive_message(get_message(client_sock))
+        client_socket = socket(AF_INET, SOCK_STREAM)
+        client_socket.connect((server_ip, server_port))
+        send_message(client_socket, confirm_presence(client_name))
+        answer = receive_message(get_message(client_socket))
         LOG.info(
             f'Connection with server {server_ip}:{server_port}. '
             f'Server response: {answer}'
@@ -164,11 +171,11 @@ def main():
         LOG.error(f'Failed decoding of JSON message.')
         sys.exit(1)
     else:
-        receiver = Thread(target=server_message_handler, args=(client_sock, client_name))
+        receiver = ClientReader(client_name, client_socket)
         receiver.daemon = True
         receiver.start()
 
-        sender = Thread(target=user_menu, args=(client_sock, client_name))
+        sender = ClientSender(client_name, client_socket)
         sender.daemon = True
         sender.start()
 
--- server.py
+++ server.py
@@ -7,81 +7,16 @@ from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from logs import server_logger_config
 from decorators import log_decorator
 from common.variables import DEFAULT_IP_ADDRESS, DEFAULT_PORT, \
-                             DEFAULT_MAX_QUEUE_LENGTH, ACTION, PRESENCE, TIME, \
-                             ERROR, USER, MESSAGE, MESSAGE_TEXT, SENDER, \
-                             RESPONSE_200, RESPONSE_300, RESPONSE_400, \
-                             EXIT, RECIPIENT
+    DEFAULT_MAX_QUEUE_LENGTH, ACTION, PRESENCE, TIME, \
+    ERROR, USER, MESSAGE, MESSAGE_TEXT, SENDER, \
+    RESPONSE_200, RESPONSE_300, RESPONSE_400, \
+    EXIT, RECIPIENT
 
 from common.utils import get_message, send_message
-
+from metaclasses import ServerVerifier
+from descriptors import Port
 
 LOG = getLogger('server_logger')
-
-
-@log_decorator
-def processing_message(data, client, message_list, clients, names):
-    LOG.debug(f'Handle client message - {data}')
-
-    if ACTION in data and data[ACTION] == PRESENCE and TIME in data \
-        and USER in data:
-        if data[USER] not in names.keys():
-            names[data[USER]] = client
-            send_message(client, RESPONSE_200)
-            LOG.info(
-                f'Client {client.getpeername()} connected. '
-                f'Response sended {RESPONSE_200}'
-            )
-        else:
-            response = RESPONSE_400
-            response[ERROR] = 'Username is not available.'
-            send_message(client, response)
-            LOG.error(
-                f'Attempt connection user {client.getpeername()} with '
-                f'not available username. Sended response {RESPONSE_400}'
-            )
-            clients.remove(client)
-            client.close()
-        return
-    elif ACTION in data and data[ACTION] == MESSAGE and TIME in data and SENDER in data and RECIPIENT in data and MESSAGE_TEXT in data:
-        message_list.append(data)
-        return
-    elif ACTION in data and data[ACTION] == EXIT and TIME in data and USER in data:
-        LOG.info(
-            f'Client {client.getpeername()} disconnected from the server.'
-        )
-        clients.remove(names[data[USER]])
-        names[data[USER]].close()
-        del names[data[USER]]
-        return
-    else:
-        response = RESPONSE_400
-        response[ERROR] = 'Bad request.'
-        LOG.debug(
-            f'Have got incorrect request from the client {client.getpeername()}, '
-            f'sended response {response}'
-        )
-        send_message(client, response)
-        return
-
-
-@log_decorator
-def message_handler(message, names, listen_sock):
-
-    if message[RECIPIENT] in names and names[message[RECIPIENT]] in listen_sock:
-        send_message(names[message[RECIPIENT]], message)
-        LOG.info(
-            f'Message from the client {message[SENDER]} '
-            f'sended to adressee {message[RECIPIENT]}.'
-        )
-    elif message[RECIPIENT] in names and names[message[RECIPIENT]] not in listen_sock:
-        raise ConnectionError
-    else:
-        no_user_dict = RESPONSE_300
-        no_user_dict[ERROR] = f'User with name {message[RECIPIENT]} ' \
-                              f'not registered on the server.'
-        send_message(names[message[SENDER]], no_user_dict)
-        LOG.error(f'{no_user_dict[ERROR]} Sending message is not available.')
-        LOG.debug(f'User {message[SENDER]} get the response {no_user_dict}')
 
 
 @log_decorator
@@ -90,71 +25,143 @@ def args_parser():
     parser.add_argument('-p', default=DEFAULT_PORT, type=int)
     parser.add_argument('-a', default=DEFAULT_IP_ADDRESS)
     namespace = parser.parse_args(sys.argv[1:])
-    if not 1023 < namespace.p < 65536:
-        LOGG.critical(
-            f'Incorrect enttered port "{namespace.p}".'
-        )
-        sys.exit(1)
     return namespace.a, namespace.p
+
+
+class Server(metaclass=ServerVerifier):
+    port = Port()
+
+    def __init__(self, listen_address, listen_port):
+        self.socket = None
+        self.addr = listen_address
+        self.port = listen_port
+
+        self.clients = []
+        self.messages = []
+        self.names = dict()
+
+    def init_socket(self):
+        LOG.info(
+            f'Server ran with parameters - {self.addr}:{self.port}'
+        )
+
+        server_sock = socket(AF_INET, SOCK_STREAM)
+        server_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        server_sock.bind((self.addr, self.port))
+        server_sock.settimeout(0.5)
+
+        self.socket = server_sock
+        self.socket.listen(DEFAULT_MAX_QUEUE_LENGTH)
+
+    def main_loop(self):
+        self.init_socket()
+
+        while True:
+            try:
+                client_sock, client_address = self.socket.accept()
+            except OSError:
+                pass
+            else:
+                LOG.info(f'Connection with client {client_address} established.')
+                self.clients.append(client_sock)
+                print(self.clients)
+
+            clients_senders = []
+            clients_addressees = []
+            errors_list = []
+
+            try:
+                if self.clients:
+                    clients_senders, clients_addressees, errors_list = select(self.clients, self.clients, [], 0)
+            except OSError:
+                print('Exception OSError - line 75')
+
+            if clients_senders:
+                for client_with_message in clients_senders:
+                    try:
+                        self.processing_message(get_message(client_with_message), client_with_message)
+                    except Exception:
+                        LOG.info(
+                            f'Client {client_with_message.getpeername()} disconnected from the server.'
+                        )
+                        self.clients.remove(client_with_message)
+            for message in self.messages:
+                try:
+                    self.message_handler(message, clients_addressees)
+                except Exception:
+                    LOG.info(f'Client {message[RECIPIENT]} disconnected from the server.')
+                    self.clients.remove(self.names[message[RESIPIENT]])
+                    del self.names[message[RECIPIENT]]
+            self.messages.clear()
+
+    def processing_message(self, data, client):
+        LOG.debug(f'Handle client message - {data}')
+
+        if ACTION in data and data[ACTION] == PRESENCE and TIME in data and USER in data:
+            if data[USER] not in self.names.keys():
+                self.names[data[USER]] = client
+                send_message(client, RESPONSE_200)
+                LOG.info(
+                    f'Client {client.getpeername()} connected. '
+                    f'Response sended {RESPONSE_200}'
+                )
+            else:
+                response = RESPONSE_400
+                response[ERROR] = 'Username is not available.'
+                send_message(client, response)
+                LOG.error(
+                    f'Attempt connection user {client.getpeername()} with '
+                    f'not available username. Sended response {RESPONSE_400}'
+                )
+                self.clients.remove(client)
+                client.close()
+            return
+        elif ACTION in data and data[
+            ACTION] == MESSAGE and TIME in data and SENDER in data and RECIPIENT in data and MESSAGE_TEXT in data:
+            self.messages.append(data)
+            return
+        elif ACTION in data and data[ACTION] == EXIT and TIME in data and USER in data:
+            LOG.info(
+                f'Client {client.getpeername()} disconnected from the server.'
+            )
+            self.clients.remove(self.names[data[USER]])
+            self.names[data[USER]].close()
+            del self.names[data[USER]]
+            return
+        else:
+            response = RESPONSE_400
+            response[ERROR] = 'Bad request.'
+            LOG.debug(
+                f'Have got incorrect request from the client {client.getpeername()}, '
+                f'sended response {response}'
+            )
+            send_message(client, response)
+            return
+
+    def message_handler(self, message, listen_socks):
+        if message[RECIPIENT] in self.names and self.names[message[RECIPIENT]] in listen_socks:
+            send_message(self.names[message[RECIPIENT]], message)
+            LOG.info(
+                f'Message from the client {message[SENDER]} '
+                f'sended to adressee {message[RECIPIENT]}.'
+            )
+        elif message[RECIPIENT] in self.names and self.names[message[RECIPIENT]] not in listen_socks:
+            raise ConnectionError
+        else:
+            no_user_dict = RESPONSE_300
+            no_user_dict[ERROR] = f'User with name {message[RECIPIENT]} ' \
+                                  f'not registered on the server.'
+            send_message(self.names[message[SENDER]], no_user_dict)
+            LOG.error(f'{no_user_dict[ERROR]} Sending message is not available.')
+            LOG.debug(f'User {message[SENDER]} get the response {no_user_dict}')
 
 
 def main():
     listen_address, listen_port = args_parser()
-    LOG.info(
-        f'Server ran with parameters - {listen_address}:{listen_port}'
-    )
 
-    server_sock = socket(AF_INET, SOCK_STREAM)
-    server_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    server_sock.bind((listen_address, listen_port))
-    server_sock.settimeout(0.5)
-    server_sock.listen(DEFAULT_MAX_QUEUE_LENGTH)
-
-    clients = []
-    messages = []
-    names = dict()
-
-    while True:
-        try:
-            client_sock, client_address = server_sock.accept()
-        except OSError:
-            pass
-        else:
-            LOG.info(f'Connection with client {client_address} established.')
-            clients.append(client_sock)
-
-        clients_senders = []
-        clients_addressees = []
-        errors_list = []
-
-        try:
-            if clients:
-                clients_senders, clients_addressees, errors_list = select(clients, clients, [], 0)
-        except OSError:
-            print('Exception OSError - line 116')
-
-        if clients_senders:
-            for client_with_message in clients_senders:
-                try:
-                    processing_message(get_message(client_with_message), client_with_message, messages, clients, names)
-                except Exception:
-                    LOG.info(
-                        f'Client {client_with_message.getpeername()} disconnected from the server.'
-                    )
-                    clients.remove(client_with_message)
-        for message in messages:
-            try:
-                message_handler(message, names, clients_addressees)
-            except Exception:
-                LOG.info(f'Client {message[RECIPIENT]} disconnected from the server.')
-                no_user_dict = RESPONSE_300
-                no_user_dict[ERROR] = f'Client {message[RECIPIENT]} disconnected from the server.'
-                send_message(names[message[SENDER]], no_user_dict)
-                clients.remove(names[message[RESIPIENT]])
-                del names[message[RECIPIENT]]
-        messages.clear()
+    server = Server(listen_address, listen_port)
+    server.main_loop()
 
 
 if __name__ == '__main__':
     main()
-
